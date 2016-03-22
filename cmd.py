dc1308758f36ce97cb411a41e35c7d879a2d3d07
cThana7os <941358554@qq.com>
2016-03-22 14:23:35 +0800

    Added files via upload

--- cmd.py
+++ cmd.py
@@ -4,47 +4,38 @@ import os;
 def GetPathSize(strPath):
     if not os.path.exists(strPath):
         return 0;
-
     if os.path.isfile(strPath):
         return os.path.getsize(strPath);
-
     nTotalSize = 0;
     for strRoot, lsDir, lsFiles in os.walk(strPath):
-        #get child directory size
-      #  for strDir in lsDir:
-     #        nTotalSize = nTotalSize + GetPathSize(os.path.join(strRoot, strDir));
+       for strDir in lsDir:
+            nTotalSize = nTotalSize + os.path.getsize(os.path.join(strRoot, strDir));
 
-        #for child file size
-        for strFile in lsFiles:
-            nTotalSize = nTotalSize + os.path.getsize(os.path.join(strRoot, strFile));
-
+       for strFile in lsFiles:
+            nTotalSize = nTotalSize + GetPathSize(os.path.join(strRoot, strFile));
     return nTotalSize;
-def getFileList( p ):
 
-        p = str( p )
-
+def getFileList(p):
+        p = str(p)
         if p=="":
-
               return [ ]
-
         p = p.replace( "/","\\")
-
-        if p[ -1] != "\\":
-
+        if p[-1] != "\\":
              p = p+"\\"
-
-        a = os.listdir( p )
-
-        b = [ x   for x in a if os.path.isdir( p + x ) ]
-
+        a = os.listdir(p)
+        b = [x for x in a if os.path.isdir(p + x)]
         return b
-
-
-make = getFileList( "C:\\" )
+file=open('out.txt','w')
+PATH = sys.argv[0][0:-6]
+PATH = "C:\\MAts\\"      #for test
+make = getFileList(PATH)
 for em in make:
-     nFileSize =round( GetPathSize("c:\\"+em)/1024.0/1024.0/1024.0,2)
-     print em;
-     print(round(nFileSize/1024.0/1024.0/1024.0,3))
+     nFileSize =round( GetPathSize(PATH+em)/1024.0/1024.0/1024.0,3)
+     if not nFileSize == 0:
+         print em
+         print(str(nFileSize)+"G")
+         file.write(em+":\n"+str(nFileSize)+"G\n",)
+file.close()
 # if __name__ == '__main__':
 #     nFileSize = GetPathSize(sys.argv[1])
 #     print(round(nFileSize/1024.0/1024.0/1024.0,3));
